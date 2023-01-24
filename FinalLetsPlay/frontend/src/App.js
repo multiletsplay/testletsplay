2f9700abef2fc416705413fed087ed495245d197
KAI <a90605190@gmail.com>
2023-01-24 14:56:56 +0900

    nodde

--- FinalLetsPlay/frontend/src/App.js
+++ FinalLetsPlay/frontend/src/App.js
@@ -13,21 +13,9 @@ function App() {
                 setMessage(message);
             });
     },[])
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <h1 className="App-title">{message}</h1>
-            </header>
-            <p className="App-intro">
-                To get started, edit <code>src/App.js</code> and save to reload.
-            </p>
-        </div>
-    )
   return (
     <Layout message={message}/>
   );
->>>>>>> branch 'main' of https://github.com/multiletsplay/testletsplay.git
 }
 
 export default App;
