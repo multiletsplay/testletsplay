f7e736d241896977a63f82c642866b13d42d096a
goyuna <ggyn210@gmail.com>
2023-01-24 12:33:16 +0900

    test commit

--- FinalLetsPlay/src/main/java/com/letplay/letplaytest/controller/LetsYunaController.java
+++ FinalLetsPlay/src/main/java/com/letplay/letplaytest/controller/LetsYunaController.java
@@ -54,7 +54,7 @@ public class LetsYunaController {
 	@Autowired
 	private LikesBiz likesBiz;
 	
-	@GetMapping("/api/hello")
+	@GetMapping("/home")
     public String hello(){
         return "안녕하세요. 현재 서버시간은 "+new Date() +"입니다. \n";
     }
